16e86ea1773e7425aeb73139b0e0de1589b8ef96
Allyson Park <allysonpark@berkeley.edu>
2021-09-07 23:37:16 -0700

    Release hw2 skeleton

---
+++ hw2/arrays/Arrays.java
@@ -0,0 +1,42 @@
+package arrays;
+
+/* NOTE: The file Arrays/Utils.java contains some functions that may be useful
+ * in testing your answers. */
+
+/** HW #2 */
+
+/** Array utilities.
+ *  @author
+ */
+class Arrays {
+
+    /* C1. */
+    /** Returns a new array consisting of the elements of A followed by the
+     *  the elements of B. */
+    static int[] catenate(int[] A, int[] B) {
+        /* *Replace this body with the solution. */
+        return null;
+    }
+
+    /* C2. */
+    /** Returns the array formed by removing LEN items from A,
+     *  beginning with item #START. If the start + len is out of bounds for our array, you
+     *  can return null.
+     *  Example: if A is [0, 1, 2, 3] and start is 1 and len is 2, the
+     *  result should be [0, 3]. */
+    static int[] remove(int[] A, int start, int len) {
+        /* *Replace this body with the solution. */
+        return null;
+    }
+
+    /* C3. */
+    /** Returns the array of arrays formed by breaking up A into
+     *  maximal ascending lists, without reordering.
+     *  For example, if A is {1, 3, 7, 5, 4, 6, 9, 10}, then
+     *  returns the three-element array
+     *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
+    static int[][] naturalRuns(int[] A) {
+        /* *Replace this body with the solution. */
+        return null;
+    }
+}
