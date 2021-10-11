2d1ad53c14ee032c9da1bb7acab7c8843a6be2ce
Shreyas Kompalli <shreyaskompalli@berkeley.edu>
2021-10-10 21:23:20 -0700

    Update proj2

---
+++ proj2/jump61/AI.java
@@ -0,0 +1,81 @@
+// This file contains definitions for an OPTIONAL part of your project.  If you
+// choose not to do the optional point, you can delete this file from your
+// project.
+
+// This file contains a SUGGESTION for the structure of your program.  You
+// may change any of it, or add additional files to this directory (package),
+// as long as you conform to the project specification.
+
+// Comments that start with "//" are intended to be removed from your
+// solutions.
+package jump61;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static jump61.Side.*;
+
+/** An automated Player.
+ *  @author P. N. Hilfinger
+ */
+class AI extends Player {
+
+    /** A new player of GAME initially COLOR that chooses moves automatically.
+     *  SEED provides a random-number seed used for choosing moves.
+     */
+    AI(Game game, Side color, long seed) {
+        super(game, color);
+        _random = new Random(seed);
+    }
+
+    @Override
+    String getMove() {
+        Board board = getGame().getBoard();
+
+        assert getSide() == board.whoseMove();
+        int choice = searchForMove();
+        getGame().reportMove(board.row(choice), board.col(choice));
+        return String.format("%d %d", board.row(choice), board.col(choice));
+    }
+
+    /** Return a move after searching the game tree to DEPTH>0 moves
+     *  from the current position. Assumes the game is not over. */
+    private int searchForMove() {
+        Board work = new Board(getBoard());
+        int value;
+        assert getSide() == work.whoseMove();
+        _foundMove = -1;
+        if (getSide() == RED) {
+            value = 0; // FIXME
+        } else {
+            value = 0; // FIXME
+        }
+        return _foundMove;
+    }
+
+
+    /** Find a move from position BOARD and return its value, recording
+     *  the move found in _foundMove iff SAVEMOVE. The move
+     *  should have maximal value or have value > BETA if SENSE==1,
+     *  and minimal value or value < ALPHA if SENSE==-1. Searches up to
+     *  DEPTH levels.  Searching at level 0 simply returns a static estimate
+     *  of the board value and does not set _foundMove. If the game is over
+     *  on BOARD, does not set _foundMove. */
+    private int minMax(Board board, int depth, boolean saveMove,
+                       int sense, int alpha, int beta) {
+        return 0; // FIXME
+    }
+
+    /** Return a heuristic estimate of the value of board position B.
+     *  Use WINNINGVALUE to indicate a win for Red and -WINNINGVALUE to
+     *  indicate a win for Blue. */
+    private int staticEval(Board b, int winningValue) {
+        return 0; // FIXME
+    }
+
+    /** A random-number generator used for move selection. */
+    private Random _random;
+
+    /** Used to convey moves discovered by minMax. */
+    private int _foundMove;
+}
